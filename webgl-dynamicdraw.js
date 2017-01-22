3924232e75e8f260fa42b1535239599916f6d2a1
coffeenotfound <coffeenotfound@users.noreply.github.com>
2017-01-22 17:01:03 +0100

    Purge old code

--- webgl-dynamicdraw.js
+++ webgl-dynamicdraw.js
@@ -16,220 +16,54 @@ var WebGLDynamicDraw = {
 // context contains cached state
 WebGLDynamicDraw.DynamicDrawContext = function(gl) {
 	this.gl = gl;
-	
-	this.state.attribs = new Array(gl.getParameter(gl.MAX_VERTEX_ATTRIBS));
-	this.state.recordArrayAttribOffsets = new Array(gl.getParameter(gl.MAX_VERTEX_ATTRIBS));
 };
 WebGLDynamicDraw.DynamicDrawContext.prototype = {
 	
-	// ### NOTES ###
-	// - mapping doesn't exist in webgl
-	// - buffersubdata causes an implicit flush
-	//   - consider flip-flopping two buffers
-	//	   - multiple start/ends a frame cycle through multiple buffers while still not drawn -> implicit flush again
-	// - orphaning seems to be the best bet
-	
-	state: {
-		// config state
-		attribs: [],
-		
-		// state
-		recordArray: new Float32Array(256),
-		bufferSize: 256,
-		bufferTypeBytes: 4,
-		bufferPos: 0,
-		buffer: null,
-		
-		// temporary state
-		inStartEnd: false,
-		currentPrimityMode: 0,
-		
-		recordArrayPos: 0,
-		recordArrayStartPos: 0,
-		recordArrayVertexStride: 0,
-		recordArrayPrimitiveStride: 0,
-		recordArrayAttribOffsets: [],
-		recordAraryVertexIndex: 0,
-	},
-	
 	/** Starts recording vertices with the given  */
 	begin: function(primitivemode) {
-		// error: in start/end
-		if(this.state.inStartEnd) throw "cannot invoke begin while in start/end";
 		
-		// set flag
-		this.state.inStartEnd = true;
-		this.state.currentPrimityMode = primitivemode;
-		
-		// calc recordArray attrib offsets
-		this.state.recordArrayVertexStride = 0;
-		for(var i = 0; i < this.state.attribs.length; i++) {
-			var attrib = this.state.attribs[i];
-			
-			if(attrib && attrib.enabled) {
-				this.state.recordArrayAttribOffsets[i] = this.state.recordArrayVertexStride;
-				this.state.recordArrayVertexStride += attrib.size;
-			}
-			else {
-				this.state.recordArrayAttribOffsets[i] = -1;
-			}
-		}
-		
-		// calc recordArrayPrimitiveStride
-		switch(primitivemode) {
-			case gl.TRIANGLES: this.state.recordArrayPrimitiveStride = 3; break;
-			case gl.POINTS: this.state.recordArrayPrimitiveStride = 1; break;
-			case gl.LINES: this.state.recordArrayPrimitiveStride = 2; break;
-			case gl.QUADS: this.state.recordArrayPrimitiveStride = 4; break;
-			default: throw "unimplemented primitive stride";
-		}
-		
-		this.state.recordArrayStartPos = this.state.recordArrayPos;
 	},
 	
 	end: function() {
-		var gl = this.gl;
 		
-		// error: not in start/end
-		if(!this.state.inStartEnd) return;
-		
-		// flush draw
-		var drawFirst = this.state.recordArrayStartPos;
-		var drawCount = this.state.recordArrayPos - this.state.recordArrayStartPos;
-		this._flushDraw(drawFirst, drawCount);
-		
-		// set flag
-		this.state.inStartEnd = false;
 	},
 	
 	/** Adds a vertex with three components to the vertex attribute attrib */
 	addVertex3: function(index, x, y, z) {
-		// ensure attrib enabled
-		var attrib = this.state.attribs[index];
-		if(!attrib || !attrib.enabled) throw "invalid attrib index: attrib " + index + " invalid or disabled";
-		
-		// put vertex
-		var writeOffset = this.state.recordArrayPos + this.state.recordArrayAttribOffsets[index];
-		this.state.recordArray[writeOffset + 0] = x;
-		this.state.recordArray[writeOffset + 1] = y;
-		this.state.recordArray[writeOffset + 2] = z;
-		
-		// increment pos if attrib 0 (0 is the incrementing attrib)
-		if(index == 0) {
-			this.state.recordArrayPos += this.state.recordArrayVertexStride;
-			
-			// if full, trigger draw and clear for next vertex add
-			if(this.state.recordArrayPos > this.state.recordArray.length) {
-				var drawFirst = this.state.recordArrayStartPos;
-				var drawCount = this.state.recordArrayPos - this.state.recordArrayStartPos;
-				this._flushDraw(drawFirst, drawCount);
-				
-				// reset recordArray
-				this.state.recordArrayStartPos = 0;
-				this.state.recordArrayPos = 0;
-			}
-		}
-	},
-	
-	// TODO: implement functions
-	/** Adds one or more vertices with three components from the given (typed) array to the vertex attribute attrib */
-	addVertices3: function(attrib, vertices, count, offset, stride) {
-		
-	},
-	addVerticesOffset3: function(attrib, vertices, count, offset, stride) {
 		
 	},
 	
 	/** Changes the configuration of vertex attribute */
 	vertexAttrib: function(index, size, type, normalized) {
-		// error: in start/end
-		if(this.state.inStartEnd) throw "cannot change attrib while in start/end";
 		
-		// get attrib
-		var attrib = this._ensureGetAttrib(index);
-		
-		// update
-		attrib._update(size, type, normalized);
 	},
 	
 	enableVertexAttrib: function(index, enabled) {
-		var attrib = this._ensureGetAttrib(index);
 		
-		attrib.enabled = enabled;
 	},
 	
 	/** Initializes or re-initialized this context with the current state. */
 	init: function() {
 		
 	},
-	
-	_flushDraw: function(first, count) {
-		var gl = this.gl;
-		
-		// ensure buffer exists
-		if(!this.state.buffer) {
-			this.state.buffer = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, this.state.buffer);
-			gl.bufferData(gl.ARRAY_BUFFER, this.state.bufferSize*this.state.bufferTypeBytes, gl.DYNAMIC_DRAW);
-			
-			// DEBUG: log
-			console.debug("created buffer " + this.state.buffer);
-		}
-		
-		// bind buffer
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.state.buffer);
-		
-		var componentsToDraw = count;
-		
-		while(componentsToDraw > 0) {
-			var numIndicesCanDrawNow = Math.floor((this.state.bufferSize - this.state.bufferPos) / this.state.recordArrayVertexStride);
-			if(numIndicesCanDrawNow > (count / this.state.recordArrayVertexStride)) numIndicesCanDrawNow = (count / this.state.recordArrayVertexStride);
-			
-			// upload
-			var dataToUpload = this.state.recordArray.subarray(this.state.recordArrayStartPos, this.state.recordArrayPos);
-			gl.bufferSubData(gl.ARRAY_BUFFER, this.state.bufferPos*this.state.bufferTypeBytes, dataToUpload);
-			
-			// setup vertexattribpointers
-			for(var j = 0; j < this.state.recordArrayAttribOffsets.length; j++) {
-				var offset = this.state.recordArrayAttribOffsets[j];
-				
-				if(offset >= 0) {
-					var attrib = this.state.attribs[j];
-					
-					gl.vertexAttribPointer(j, attrib.size, attrib.type, attrib.normalized, this.state.recordArrayVertexStride*this.state.bufferTypeBytes, this.state.bufferPos*this.state.bufferTypeBytes);
-				}
-			}
-			
-			// actually do the draw
-			gl.drawArrays(this.state.currentPrimitiveMode, 0, numIndicesCanDrawNow);
-			
-			// increment/decrement
-			this.state.bufferPos += dataToUpload.length;
-			componentsToDraw -= numIndicesCanDrawNow * this.state.recordArrayVertexStride;
-			
-			// orphan
-			if(componentsToDraw >= 0) {
-				//gl.bufferData(gl.ARRAY_BUFFER, null, gl.DYNAMIC_DRAW);
-				gl.bufferData(gl.ARRAY_BUFFER, this.state.bufferSize*this.state.bufferTypeBytes, gl.DYNAMIC_DRAW);
-				
-				this.state.bufferPos = 0;
-				continue;
-			}
-		}
-	},
-	
-	_ensureGetAttrib: function(index) {
-		// check index range
-		// if(that) throw "invalid vertex attrib index: " + index;
-		
-		// ensure
-		var attrib = this.state.attribs[index];
-		if(!attrib) {
-			attrib = this.state.attribs[index] = new WebGLDynamicDraw.VertexAttrib(index);
-		}
-		return attrib;
-	},
 };
+
+
+WebGLDynamicDraw.RecordArray = function(size) {
+	
+};
+WebGLDynamicDraw.RecordArray.prototype = {
+	
+};
+
+
+WebGLDynamicDraw.DrawBuffer = function(size) {
+	this.size = size; // size in components
+};
+WebGLDynamicDraw.DrawBuffer.prototype = {
+	
+};
+
 
 WebGLDynamicDraw.VertexAttrib = function(index) {
 	this.index = index;
@@ -249,8 +83,3 @@ WebGLDynamicDraw.VertexAttrib.prototype = {
 	},
 };
 
-/*
-WebGLDynamicDraw.QueuedDraw = function() {
-	
-};
-*/
